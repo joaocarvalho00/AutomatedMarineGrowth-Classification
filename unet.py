6714b49d15c2c42679fd6bcd6d7180d96eaa1bd4
joaocarvalho00 <joaoafonsocarvalho00@gmail.com>
2023-02-22 12:47:55 +0000

    first unet model

---
+++ unet.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+import cv2
+from hyperparameters import *
+from torchvision.models import vgg16_bn
+
+
+
+class UNet(nn.Module):
+    def __init__(self, pretrained=True, out_channels=12):
+        super().__init__()
+
+        # Encoding
+        self.encoder = vgg16_bn(pretrained=pretrained).features   #nn.Sequential behaves like python list of all layers (so can be sliced)
+        self.block1 = nn.Sequential(*self.encoder[:6])
+        self.block2 = nn.Sequential(*self.encoder[6:13])
+        self.block3 = nn.Sequential(*self.encoder[13:20])
+        self.block4 = nn.Sequential(*self.encoder[20:27])
+        self.block5 = nn.Sequential(*self.encoder[27:34])
+
+        self.bottleneck = nn.Sequential(*self.encoder[34:])
+        self.conv_bottleneck = conv(512, 1024)
+
+        # Decoding
+        self.up_conv6 = up_conv(1024, 512)
+        self.conv6 = conv(512 + 512, 512)
+        self.up_conv7 = up_conv(512, 256)
+        self.conv7 = conv(256 + 512, 256)
+        self.up_conv8 = up_conv(256, 128)
+        self.conv8 = conv(128 + 256, 128)
+        self.up_conv9 = up_conv(128, 64)
+        self.conv9 = conv(64 + 128, 64)
+        self.up_conv10 = up_conv(64, 32)
+        self.conv10 = conv(32 + 64, 32)
+
+        # Output layer
+        self.conv11 = nn.Conv2d(32, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        block1 = self.block1(x)
+        block2 = self.block2(block1)
+        block3 = self.block3(block2)
+        block4 = self.block4(block3)
+        block5 = self.block5(block4)
+
+        bottleneck = self.bottleneck(block5)
+        x = self.conv_bottleneck(bottleneck)
+
+        x = self.up_conv6(x)
+        x = torch.cat([x, block5], dim=1) # skip connection
+        x = self.conv6(x)
+
+        x = self.up_conv7(x)
+        x = torch.cat([x, block4], dim=1) # skip connection
+        x = self.conv7(x)
+
+        x = self.up_conv8(x)
+        x = torch.cat([x, block3], dim=1) # skip connection
+        x = self.conv8(x)
+
+        x = self.up_conv9(x)
+        x = torch.cat([x, block2], dim=1) # skip connection
+        x = self.conv9(x)
+
+        x = self.up_conv10(x)
+        x = torch.cat([x, block1], dim=1) # skip connection
+        x = self.conv10(x)
+
+        x = self.conv11(x)
+
+        return x
+
+
+
+
