432d86e3b01d8848aa829e0f2c97cfe5b60b4b90
joaocarvalho00 <joaoafonsocarvalho00@gmail.com>
2023-02-22 19:20:55 +0000

    added plot_stats function
    plot_stats plots acc and loss

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from torchvision import transforms
+import matplotlib.pyplot as plt
 
 
 def get_transforms():
@@ -12,3 +13,23 @@ def get_transforms():
                                  [0.229, 0.224, 0.225]
                                  ) # for imagenet
                              ])
+
+def plot_stats(train_loss, train_acc, val_loss, val_acc):
+    
+    fig, (ax1, ax2) = plt.subplots(2)
+        
+    ax1.plot(train_loss, label="train_loss")
+    ax1.plot(val_loss, label="val_loss")
+    ax1.set_title("Loss")
+    ax1.set_xlabel("epochs")
+    ax1.legend()
+
+    ax2.plot(train_acc, label="train_acc")
+    ax2.plot(val_acc, label="val_acc")
+    ax2.set_title("Accuracy")
+    ax2.set_xlabel("epochs")
+    ax2.legend()
+
+
+    plt.tight_layout()
+    plt.show()
